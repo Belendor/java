55f67f7bf7a2dd1311167b70afb08a31c14cd28f
Belendor <dwarf11@gmail.com>
2023-02-13 21:10:35 +0200

    Invoice update

--- Invoice.java
+++ Invoice.java
@@ -19,6 +19,10 @@ public class Invoice {
         this.partNumber = partNumber;
     }
 
+    public static void main(String[] args) {
+        testInvoice(args); //will run tests
+    }
+
     public String getPartNumber() {
         return partNumber;
     }
@@ -55,5 +59,14 @@ public class Invoice {
     public int getInvoiceAmount() {
         // then returns the amount as an int value.
         return quantity * pricePerItem;
+    }
+
+    public static void testInvoice(String[] args) {
+        Invoice invoice = new Invoice("1234", "Screwdriver", 5, 10);
+        System.out.println("Part Number: " + invoice.getPartNumber());
+        System.out.println("Part Description: " + invoice.getPartDescription());
+        System.out.println("Quantity: " + invoice.getQuantity());
+        System.out.println("Price Per Item: " + invoice.getPricePerItem());
+        System.out.println("Invoice Amount: " + invoice.getInvoiceAmount());
     }
 }
